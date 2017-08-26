75ffa3313b280058830b9367f436afe059611283
Logan Jerrow <logan.jerrow@gmail.com>
2017-08-26 13:34:38 -0700

    Setting up Base project

---
+++ LJ.Receipt.Base/ViewModels/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LJ.Receipt.Base.ViewModels
+{
+    public class Class1
+    {
+    }
+}
